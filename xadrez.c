e6a757427897aa22633073846d3f0db36b6a0b0f
Thiago Vieira da Paz <programador@programador.me>
2025-05-22 09:45:51 +0000

    feat: finalizado desafio nivel mestre

--- xadrez.c
+++ xadrez.c
@@ -2,42 +2,72 @@
 
 // Desafio de Xadrez - MateCheck
 
+void moverTorre(int casas) {
+    if(casas > 0) {
+        printf("Direita \n");
+        moverTorre(casas - 1);
+    }
+}
+
+void moverBispo(int linha, int coluna, int maxCasas) {
+    if (linha >= maxCasas || coluna >= maxCasas) {
+        return;
+    }
+
+    for (int i = linha; i < maxCasas; i++) {
+        for (int j = coluna; j < maxCasas; j++) {
+            if (i == j) {
+                printf("Cima, Direita \n");
+                break;
+            }
+        }
+        moverBispo(i + 1, coluna + 1, maxCasas); 
+        break;
+    }
+}
+
+void moverRainha(int casas) {
+    if(casas > 0) {
+        printf("Esquerda \n");
+        moverRainha(casas - 1);
+    }
+}
+
+void moverCavalo() {
+    int movimentos = 0;
+
+    for (int i = 0; i < 8; i++) {   
+        for (int j = 0; j < 8; j++) {  
+            if ((i + 2 < 8) && (j + 1 < 8)) {
+                if (movimentos >= 2) break; 
+                printf("Cima \n");   
+                movimentos++;
+                continue;
+            }
+        }
+        printf("Direita \n");   
+        if (movimentos >= 2) break;
+    }
+}
+
 int main() {
     // Torre
     printf("Torre \n");
-    for (int t = 0; t < 5; t++)
-    {
-        printf("Direita \n");
-    }
-    
+    moverTorre(5);
+ 
     // Bispo
     printf("Bispo \n");
-    int b = 0;
-    while(b < 5) {
-        printf("Cima, Direita \n");
-        b++;
-    }
+    moverBispo(0, 0, 5);
+   
 
     // Rainha
     printf("Rainha \n");
-    int r = 0;
-    do {
-        printf("Esquerda \n");
-        r++;
-    }
-    while (r < 8);
+    moverRainha(8);
 
     // Cavalo
     printf("Cavalo \n");
-    int cx = 0, cy = 0;
-
-    for(int cy = 0; cy < 1; cy++) {
-        while(cx < 2) {
-            printf("Cima \n");
-            cx++;
-        }
-        printf("Direita \n");
-    }
+    moverCavalo();
+    
     
 
     return 0;
